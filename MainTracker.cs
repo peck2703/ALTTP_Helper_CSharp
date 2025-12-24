966a99374ab7a0f356478f23e625526ffd2bf92b
Michael R Peck Jr <mrpeckjr@gmail.com>
2025-12-23 22:53:20 -0600

    Add project files.

---
+++ MainTracker.cs
@@ -0,0 +1,15 @@
+namespace ALTTP_Helper_CSharp
+{
+    public partial class MainTracker : Form
+    {
+        public MainTracker()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTracker_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
